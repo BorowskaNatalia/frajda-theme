e540d49189bb50d43e47541e133337e718980067
NataliaBorowskaPJATK <s25566@pjwstk.edu.pl>
2025-07-05 13:18:28 +0200

    Start motywu Frajda

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+function frajda_enqueue_styles() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'frajda_enqueue_styles');
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+?>
+
+<h1>Witaj w Stajni Frajda!</h1>
+
+<?php
+get_footer();
+?>

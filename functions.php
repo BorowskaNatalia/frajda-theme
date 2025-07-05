e7bbccbbcd38fda549039e698dae923cdef69cf9
NataliaBorowskaPJATK <s25566@pjwstk.edu.pl>
2025-07-05 14:18:25 +0200

    Dodano header z menu i podstawowym stylem

--- functions.php
+++ functions.php
@@ -3,3 +3,10 @@ function frajda_enqueue_styles() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'frajda_enqueue_styles');
+
+function frajda_setup() {
+  register_nav_menus(array(
+    'main_menu' => 'Główne menu',
+  ));
+}
+add_action('after_setup_theme', 'frajda_setup');

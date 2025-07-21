df069b1a2c24e3bc8944fc682ea6c010356bd943
NataliaBorowskaPJATK <s25566@pjwstk.edu.pl>
2025-07-21 21:00:28 +0200

    Update styles and mixins for improved header and button design

--- functions.php
+++ functions.php
@@ -2,6 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 require get_template_directory() . '/inc/enqueue.php';
 
+add_filter('nav_menu_link_attributes', function($atts, $item, $args) {
+    if (in_array('btn', $item->classes)) {
+        $atts['class'] = (isset($atts['class']) ? $atts['class'].' ' : '') . 'btn';
+    }
+    return $atts;
+}, 10, 3);
+
+
 function sf_register_menus() {
     register_nav_menus([
         'main_menu' => __('Menu główne', 'stajniafrajda'),
